74b8b8d005c7eec92566eb748c5b20c3b4a3f3eb
stuart.welham@gmail.com <stuart.welham@gmail.com>
2017-01-21 22:18:04 +0000

    added unstore function to ase gateway

--- test/cashier_test.exs
+++ test/cashier_test.exs
@@ -67,6 +67,18 @@ defmodule CashierTest do
     assert result == {:ok, "store from test_gateway"}
   end
 
+  test "unstore/1 should call into default gateway" do
+    result = Cashier.unstore(nil)
+
+    assert result == {:ok, "unstore from dummy_gateway"}
+  end
+
+  test "unstore/2 should call into a specified gateway", %{gateway: gateway} do
+    result = Cashier.unstore(nil, [gateway: gateway])
+
+    assert result == {:ok, "unstore from test_gateway"}
+  end
+
   test "void/1 should call into default gateway" do
     result = Cashier.void("")
 
