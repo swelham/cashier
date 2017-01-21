f1984b866db73eae8597eee89f3f0c50ea90d3d2
stuart.welham@gmail.com <stuart.welham@gmail.com>
2017-01-21 22:18:04 +0000

    added unstore function to ase gateway

--- lib/gateways/dummy.ex
+++ lib/gateways/dummy.ex
@@ -16,6 +16,9 @@ defmodule Cashier.Gateways.Dummy do
   def store(_, _, _),
     do: respond("store")
 
+  def unstore(_, _, _),
+    do: respond("unstore")
+
   def void(_, _, _),
     do: respond("void")
 
