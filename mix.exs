3d75bb08f03f9ab09f7af465a2c8e24e4358d60d
stuart.welham@gmail.com <stuart.welham@gmail.com>
2017-02-12 13:24:41 +0000

    bump version to 0.4.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Cashier.Mixfile do
 
   def project do
     [app: :cashier,
-     version: "0.3.0",
+     version: "0.4.0",
      elixir: "~> 1.4",
      description: description(),
      package: package(),
